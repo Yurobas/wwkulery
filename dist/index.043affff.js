4dbc905fa0258632d2aa95e9f00e8cdcc18e020c
Yura Goldman <yura_goldman@mail.ru>
2022-07-05 11:11:01 +0300

    main

--- dist/index.043affff.js
+++ dist/index.043affff.js
@@ -10,17 +10,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
             ...document.querySelectorAll('.form__phone')
         ];
         forms.forEach((form)=>{
-            const submit = form.querySelector('[type="submit"]');
-            const policy = form.querySelector('.form__policy-input');
-            policy.addEventListener('change', ()=>{
-                if (!policy.checked) submit.classList.add('disabled');
-                else submit.classList.remove('disabled');
-            });
+            if (form.querySelector('.form__policy-input')) {
+                const submit = form.querySelector('[type="submit"]');
+                const policy = form.querySelector('.form__policy-input');
+                policy.addEventListener('change', ()=>{
+                    if (!policy.checked) submit.classList.add('disabled');
+                    else submit.classList.remove('disabled');
+                });
+            }
         });
         labels.forEach((label)=>{
             const input = label.querySelector('.form__input');
             input.addEventListener('change', ()=>{
                 if (input.value != '' && input.value.length) label.classList.add('value');
+                else label.classList.remove('value');
             });
         });
     })();
--- src/js/base.js
+++ src/js/base.js
@@ -5,25 +5,29 @@ document.addEventListener('DOMContentLoaded', () => {
     const phones = [...document.querySelectorAll('.form__phone')]
   
     forms.forEach(form => {
-      const submit = form.querySelector('[type="submit"]')
-      const policy = form.querySelector('.form__policy-input')
+      if (form.querySelector('.form__policy-input')) {
+        const submit = form.querySelector('[type="submit"]')
+        const policy = form.querySelector('.form__policy-input')
 
-      policy.addEventListener('change', () => {
-        if (!policy.checked) {
-          submit.classList.add('disabled')
-        } else {
-          submit.classList.remove('disabled')
-        }
-      })
+        policy.addEventListener('change', () => {
+          if (!policy.checked) {
+            submit.classList.add('disabled')
+          } else {
+            submit.classList.remove('disabled')
+          }
+        })
+      }
     })
 
     labels.forEach(label => {
       const input = label.querySelector('.form__input')
       input.addEventListener('change', () => {
         if (input.value != '' && input.value.length) {
-          label.classList.add('value')
+          label.classList.add('value');
+        } else {
+          label.classList.remove('value');
         }
-      })
-    })
+      });
+    });
   }();
 })
