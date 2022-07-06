235b11abb36e945f7900c811829f9702e5e6a827
Yura Goldman <yura_goldman@mail.ru>
2022-07-06 11:51:47 +0300

    main

--- dist/index.043affff.js
+++ dist/index.043affff.js
@@ -27,6 +27,42 @@ document.addEventListener('DOMContentLoaded', ()=>{
             });
         });
     })();
+    (function more() {
+        if (document.querySelector('[data-more-link]') && document.querySelector('[data-more-content]')) {
+            let links = [
+                ...document.querySelectorAll('[data-more-link]')
+            ];
+            let contents = [
+                ...document.querySelectorAll('[data-more-content]')
+            ];
+            // если ссылка одна на странице (чтобы не делать циклы)
+            if (links.length > 0 && links.length == 1) {
+                links = links[0];
+                contents = contents[0];
+                let inner = contents.querySelector('[data-more-inner]');
+                let padding = 20;
+                let height = inner.getBoundingClientRect().height + padding + 'px';
+                links.addEventListener('click', (event)=>{
+                    event.preventDefault();
+                    if (!links.classList.contains('--active')) {
+                        links.textContent = 'Скрыть';
+                        links.classList.add('--active');
+                        contents.style.opacity = 1;
+                        contents.style.maxHeight = height;
+                        contents.style.paddingTop = `${padding}px`;
+                        contents.classList.add('--active');
+                    } else {
+                        links.textContent = 'Подробнее...';
+                        links.classList.remove('--active');
+                        contents.style.opacity = 0;
+                        contents.style.maxHeight = 0;
+                        contents.style.paddingTop = 0;
+                        contents.classList.remove('--active');
+                    }
+                });
+            }
+        }
+    })();
 });
 
 //# sourceMappingURL=index.043affff.js.map
